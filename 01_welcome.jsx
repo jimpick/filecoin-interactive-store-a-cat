b72d37e129b83e425a7ce1f903ce826e8c927d0c
Jim Pick <jim@jimpick.com>
2021-04-20 19:27:56 -0700

    Split welcome into it's own file

---
+++ 01_welcome.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function Welcome () {
+  const history = useHistory()
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h3>Filecoin Interactive Tutorial</h3>
+
+      <h1 style={{ fontSize: '300%' }}>Store a cat</h1>
+
+      <div style={{ fontSize: '500%' }}>
+        <span style={{ verticalAlign: 'top' }}>🐈 ⇨</span>{' '}
+        <img src='filecoin-logo.svg' style={{ height: '1.3em' }} />
+      </div>
+
+      <button style={{ marginTop: '1rem', fontSize: '300%' }} onClick={() => {
+        console.log('Click!')
+        history.push('/about')
+      }}>Let's go!</button>
+    </div>
+  )
+}
+
--- index.jsx
+++ index.jsx
@@ -1,21 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation
+} from 'react-router-dom'
 
-function Welcome () {
+import Welcome from './01_welcome.jsx'
+
+function Nav () {
+  const location = useLocation()
+  if (location.pathname === '/') {
+    return null
+  }
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h3>Filecoin Interactive Tutorial</h3>
-
-      <h1 style={{ fontSize: '300%' }}>Store a cat</h1>
-
-      <div style={{ fontSize: '500%' }}>
-        <span style={{ verticalAlign: 'top' }}>🐈 ⇨</span> {' '}
-        <img src='filecoin-logo.svg' style={{ height: '1.3em' }} />
-      </div>
-
-      <button style={{ marginTop: '1rem', fontSize: '300%' }}>Let's go!</button>
-    </div>
+    <nav>
+      <Link to='/'>Back to start</Link>
+      <pre>{JSON.stringify(location)}</pre>
+    </nav>
   )
 }
 
-ReactDOM.render(<Welcome />, document.getElementById('root'))
+export default function App () {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/users'>
+          <Users />
+        </Route>
+        <Route path='/'>
+          <Welcome />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function About () {
+  return <h2>About</h2>
+}
+
+function Users () {
+  return <h2>Users</h2>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
